9a2e9049b2d51cad5ffd05c0a78a5def7359d31e
samteb <sam.teboul@gmail.com>
2021-03-18 14:21:36 +0200

    Fix format

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-
 module.exports = {
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           postcssOptions: {
-            ident: 'postcss',
-            syntax: 'postcss-scss',
+            ident: "postcss",
+            syntax: "postcss-scss",
             plugins: [
-              require('postcss-import'),
-              require('tailwindcss'),
-              require('autoprefixer'),
+              require("postcss-import"),
+              require("tailwindcss"),
+              require("autoprefixer"),
             ],
           },
         },
