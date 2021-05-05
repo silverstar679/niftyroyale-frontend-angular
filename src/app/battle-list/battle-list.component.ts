048b8ba42dccfcc4ebdac1ff1c65835b0ca62034
samteb <sam.teboul@gmail.com>
2021-05-05 11:46:13 +0300

    update battle list

--- src/app/battle-list/battle-list.component.ts
+++ src/app/battle-list/battle-list.component.ts
@@ -9,16 +9,16 @@ import { OpenSeaService } from '../services/open-sea.service';
 export class BattleListComponent {
   battles = [
     {
-      address: '0x5cF380f0A5f8f5C096e5f3e0E180BA48521Ccb65',
-      name: 'NFTBattleRoyale - LAST',
+      address: '0x387b8F0a3c1d53d01750874431d26ccfB36A9442',
+      name: 'NFT Battle Royale - LAST',
     },
     {
       address: '0x5cF380f0A5f8f5C096e5f3e0E180BA48521Ccb65',
-      name: 'NFTBattleRoyale - LAST',
+      name: 'NFT Battle Royale - OLD',
     },
     {
-      address: '0x5cF380f0A5f8f5C096e5f3e0E180BA48521Ccb65',
-      name: 'NFTBattleRoyale - LAST',
+      address: '0x9a6a3E23ee281a99F81b887Dd75386f0f3d5341c',
+      name: 'NFT Battle Royale - BURNT TOKEN',
     },
   ];
 
