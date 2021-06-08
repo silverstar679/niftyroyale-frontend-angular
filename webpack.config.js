125a8f09b7c0654f9c71bb1ff730cad3cda77d6f
samteb <sam.teboul@gmail.com>
2021-06-08 15:19:34 +0300

    clean package.json + remove firebase files

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,4 @@ module.exports = {
     crypto: true,
     stream: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loader: "postcss-loader",
-        options: {
-          postcssOptions: {
-            ident: "postcss",
-            syntax: "postcss-scss",
-            plugins: [
-              require("postcss-import"),
-              require("tailwindcss"),
-              require("autoprefixer"),
-            ],
-          },
-        },
-      },
-    ],
-  },
 };
