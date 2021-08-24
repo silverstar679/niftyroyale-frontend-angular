4a9e94dd4f12a34bfdd89c61691f4f0ab5de241c
samuel <sam.teboul@gmail.com>
2021-08-24 20:30:25 +0300

    Fix image display on drop page

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,6 +1,7 @@
 export interface Contract {
   address: string;
   name: string;
+  picture: string;
 }
 
 export const CONTRACTS = {
@@ -9,7 +10,9 @@ export const CONTRACTS = {
     past: [
       {
         address: '0xCFa9BaabFF2AC2da41385E051386188f4F5BC94d',
-        name: `Nifty Royale X Lushsux - Sergey "Big Mac" Nazarov`,
+        name: 'Nifty Royale X Lushsux - Sergey "Big Mac" Nazarov',
+        picture:
+          'https://ipfs.io/ipfs/bafybeianq5jagquleaapi5axwzc5nzychnr6dfqxdkld2yztd7kzfelmr4/SergeyBigMacNazarov.jpg',
       },
     ],
   },
