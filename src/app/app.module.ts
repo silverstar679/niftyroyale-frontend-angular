22c17cfae58f771046f22f90a2deabc9d8b5ace8
samuel <sam.teboul@gmail.com>
2021-08-24 13:12:59 +0300

    Remove store module + add ngx countdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { MetamaskService } from './services/metamask.service';
 import { OpenSeaService } from './services/open-sea.service';
 import { ContractService } from './services/contract.service';
